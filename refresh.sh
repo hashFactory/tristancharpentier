ae1c4983c5333aeaa0e4133973b9cc10f70d153c
Tristan Charpentier <tristan_charpentier@hotmail.com>
2020-12-02 21:32:24 -0500

    refresh on save script

---
+++ refresh.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# fetch original and browser window ID
+CURRENT=`xdotool getactivewindow`
+WID=`xdotool search --name "Mozilla Firefox" | head -1`
+
+# switch to browser and refresh page
+xdotool windowactivate $WID
+xdotool key F5
+
+# focus back to original window
+xdotool windowactivate $CURRENT
