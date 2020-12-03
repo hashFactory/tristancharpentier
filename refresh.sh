dc199e7adc14e416e6bd8c118e5a3eebdab48d26
Tristan Charpentier <tristan_charpentier@hotmail.com>
2020-12-02 22:01:55 -0500

    better error checking

--- refresh.sh
+++ refresh.sh
@@ -3,7 +3,17 @@
 # give argument if you don't want to recompile
 if [ $# -eq 0 ]
 then
-    ./preprocess.py compile
+    # try to find repo
+    REPO=$(find ~ -type d -name "tristancharpentier")
+
+    # if couldn't find compile script inform user
+    if [ ! -f "$REPO/preprocess.py" ] then
+        echo "Could not find preprocess.py in repo"
+        echo "Guessed location: $REPO/preprocess.py"
+    done
+
+    # compile website
+    $REPO/preprocess.py compile
 fi
 
 # fetch original and browser window ID
