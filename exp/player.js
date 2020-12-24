86ae154f4ed4b0d4166e6ba22317ab3a0f12b99e
Tristan Charpentier <tristan_charpentier@hotmail.com>
2020-12-24 04:25:28 -0500

    nicer looking ui - wip

--- exp/player.js
+++ exp/player.js
@@ -1,12 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
-var player;
+
+var player, playbutton;
 
 function startplayer()
 {
  player = document.getElementById('music_player');
+ playButton = document.getElementById("play_button");
  player.controls = false;
+ playButton.addEventListener("click", handlePlayButton, false);
 }
 
+async function playAudio() {
+  try {
+    await player.play();
+    playButton.setAttribute("src", "pause.png");
+    playButton.classList.add("playing");
+  } catch(err) {
+    playButton.classList.remove("playing");
+  }
+}
+
+function handlePlayButton() {
+  if (player.paused) {
+    playAudio();
+  } else {
+    player.pause();
+    playButton.setAttribute("src", "play.png");
+    playButton.classList.remove("playing");
+  }
+}
+/*
 function play_aud()
 {
  player.play();
@@ -19,7 +42,7 @@ function stop_aud()
 {
  player.pause();
  player.currentTime = 0;
-}
+}*/
 function change_vol()
 {
  player.volume=document.getElementById("change_vol").value;
